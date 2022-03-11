e97a4a57a0473c428c626e1ad75bdd0f0e1d199b
Rudis <rudisarg@gmail.com>
2022-03-10 20:09:37 -0500

    created animation for mobile menu

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 new fullpage("#fullpage", {
     autoScrolling: true,
-    navigation: true,
+    navigation: true
 });
+
+const navAnimation = () => {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    mobileMenu.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .5}s`;
+            }
+        });
+
+        mobileMenu.classList.toggle('toggle');
+    });
+}
+
+navAnimation();
