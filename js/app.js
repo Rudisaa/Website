1fd1b88ca17571ff30b75f50003b8f88c1fe03a0
Rudis <rudisarg@gmail.com>
2022-03-10 21:44:51 -0500

    moved all .js files to a js folder

---
+++ js/app.js
@@ -0,0 +1,32 @@
+new fullpage("#fullpage", {
+    autoScrolling: true,
+    navigation: true,
+    menu: '#nav-menu',
+    anchors: ['home-page', 'about-page', 'contact-page'],
+    navigaionPosition: 'right',
+    parrallax: true
+});
+
+const navAnimation = () => {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    mobileMenu.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            }
+            else {
+                link.style.animation = `navLinkFade 0.4s ease forwards ${index / 7 + .5}s`;
+            }
+        });
+
+        mobileMenu.classList.toggle('toggle');
+    });
+}
+
+
+navAnimation();
