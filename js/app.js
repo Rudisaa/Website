b485544b16f3a353665bf871afa09a2cf00b9417
Rudis <rudisarg@gmail.com>
2022-03-17 15:18:58 -0400

    fixed mobile menu staying open when click on link

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ function mobileMenu() {
     navMenu.classList.toggle("active");
 }
 
-const navLink = document.querySelectorAll(".nav-links a");
+const navLink = document.querySelectorAll(".nav-link");
 
 navLink.forEach(n => n.addEventListener('click', closeMenu));
 
